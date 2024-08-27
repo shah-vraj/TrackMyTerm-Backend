ba728c54938a5a8dfb5f087797122705639ffd2b
Vraj Shah <vr597381@dal.ca>
2024-08-27 12:11:41 -0300

    feat: add user login API

--- src/main/kotlin/com/trackmyterm/service/AuthService.kt
+++ src/main/kotlin/com/trackmyterm/service/AuthService.kt
@@ -1,8 +1,13 @@
 package com.trackmyterm.service
 
+import com.trackmyterm.exception.InvalidPasswordException
 import com.trackmyterm.exception.UserAlreadyRegisteredException
+import com.trackmyterm.exception.UserNotFoundException
 import com.trackmyterm.repository.UserRepository
+import com.trackmyterm.request.LoginRequest
 import com.trackmyterm.request.RegisterRequest
+import com.trackmyterm.response.LoginResponse
+import com.trackmyterm.util.Constants.LOGIN_REQUEST_SUCCESS
 import com.trackmyterm.util.Constants.REGISTER_REQUEST_SUCCESS
 import com.trackmyterm.util.ResponseBody
 import com.trackmyterm.util.ResponseBody.ResultType.SUCCESS
@@ -14,14 +19,23 @@ interface AuthService {
     /**
      * Register a user into the application
      * @param registerRequest Request model for registering the user
+     * @return True if user registration is successful, False otherwise
      */
     fun registerUser(registerRequest: RegisterRequest): ResponseBody<Boolean>
+
+    /**
+     * Login the user into the application
+     * @param loginRequest User details to login
+     * @return ResponseBody of LoginResponse containing token
+     */
+    fun loginUser(loginRequest: LoginRequest): ResponseBody<LoginResponse>
 }
 
 @Service
 class AuthServiceImpl(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
+    private val jwtService: JwtService,
 ) : AuthService {
 
     override fun registerUser(registerRequest: RegisterRequest): ResponseBody<Boolean> {
@@ -31,5 +45,16 @@ class AuthServiceImpl(
         val user = registerRequest.toUser(passwordEncoder)
         userRepository.save(user)
         return ResponseBody(SUCCESS, true, REGISTER_REQUEST_SUCCESS)
+    }
+
+    override fun loginUser(loginRequest: LoginRequest): ResponseBody<LoginResponse> {
+        val user = userRepository.findByEmail(loginRequest.email)
+            ?: throw UserNotFoundException(loginRequest.email)
+        if (!passwordEncoder.matches(loginRequest.password, user.password))
+            throw InvalidPasswordException(loginRequest.email)
+
+        val token = jwtService.generateToken(user)
+        val loginResponse = LoginResponse(token)
+        return ResponseBody(SUCCESS, loginResponse, LOGIN_REQUEST_SUCCESS)
     }
 }
